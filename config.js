fc8fa339dc8a5bbda99626c525be825b37976de1
snehasodhani <77625772+snehasodhani@users.noreply.github.com>
2022-03-24 15:31:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// add SDK Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyDqXwI2857qhrJozKhvtP5eQ0dY7QmJhqA",
+    authDomain: "pro-67-f68ad.firebaseapp.com",
+    projectId: "pro-67-f68ad",
+    storageBucket: "pro-67-f68ad.appspot.com",
+    messagingSenderId: "775180824308",
+    appId: "1:775180824308:web:6f5d0872966d7155a6f8a1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
